7d6a97e0fecac804e34ba53bda00c6fdc547bf15
Angel Almahano <a.almahano.moreno@gmail.com>
2016-06-13 15:23:20 +0200

    mejorado el enroutado con locationProvider.html5Mode. De esta forma se quita el hashtag # en los links

--- js/apps/aam.js
+++ js/apps/aam.js
@@ -1,24 +1,30 @@
 angular.module('aam',['ngRoute'])
-	.config(function routes($routeProvider)
+	.config(function routes($routeProvider, $locationProvider)
 {
+	$locationProvider.html5Mode(
+		{
+			enabled:true,
+			requireBase: false
+		});
+
 	$routeProvider
-		.when('/',
+		.when('/app_angular/',
 		{
 			templateUrl:'html/main.html'
 		})	
-		.when('/borrar',
+		.when('/app_angular/borrar',
 		{
 			templateUrl:'html/borrar.html'
 		})	
-		.when('/listar',
+		.when('/app_angular/listar',
 		{
 			templateUrl:'html/listar.html'
 		})	
-		.when('/modificar',
+		.when('/app_angular/modificar',
 		{
 			templateUrl:'html/modificar.html'
 		})	
-		.when('/crear',
+		.when('/app_angular/crear',
 		{
 			templateUrl:'html/crear.html'
 		})			
