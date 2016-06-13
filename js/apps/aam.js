7fded806a867325e22bd94a09c5324bb2c5d5678
Angel Almahano <a.almahano.moreno@gmail.com>
2016-06-13 15:03:46 +0200

    añadido angular-route para el enrutado de las paginas.

--- js/apps/aam.js
+++ js/apps/aam.js
@@ -1,1 +1,29 @@
-angular.module('aam',[]);
+angular.module('aam',['ngRoute'])
+	.config(function routes($routeProvider)
+{
+	$routeProvider
+		.when('/',
+		{
+			templateUrl:'html/main.html'
+		})	
+		.when('/borrar',
+		{
+			templateUrl:'html/borrar.html'
+		})	
+		.when('/listar',
+		{
+			templateUrl:'html/listar.html'
+		})	
+		.when('/modificar',
+		{
+			templateUrl:'html/modificar.html'
+		})	
+		.when('/crear',
+		{
+			templateUrl:'html/crear.html'
+		})			
+
+		
+});
+
+	
